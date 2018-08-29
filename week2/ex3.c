36340fe01b03c33981b593cc407b6941d0e2183f
Артемий Быков <species_decipit@Artemijs-MacBook-Pro.local>
2018-08-29 20:51:19 +0300

    Add Week2/ex5 to ex3 file

--- week2/ex3.c
+++ week2/ex3.c
@@ -10,13 +10,39 @@ void print_level(int half_of_width, int number_of_stars) {
         printf(" ");
 }
 
-int main(int argc, char **argv) {
-    int height = atoi(argv[1]);
-
+void print_triangle(int height) {
     for (int i = 0; i < height; i++) {
         print_level((2 * height - 2) / 2 - i, 2 * i + 1);
         printf("%c", '\n');
     }
+}
+
+void print_half_of_triangle(int height) {
+    for (int i = 1; i <= height; i++) {
+        print_level(0, i);
+        printf("%c", '\n');
+    }
+}
+
+void print_square(int height) {
+    for (int i = 0; i < height; i++) {
+        print_level(0, height);
+        printf("%c", '\n');
+    }
+}
+
+void print_half_of_rhombus(int height) {
+    print_half_of_triangle(height);
+    for (int i = height - 1; i >= 1; i--) {
+        print_level(0, i);
+        printf("%c", '\n');
+    }
+}
+
+int main(int argc, char **argv) {
+    int height = atoi(argv[1]);
+
+    print_square(height);
 
     return 0;
 }
