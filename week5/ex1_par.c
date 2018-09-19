b39c817e9c7b314dbbd32d3f570952abab88b7c2
species_decipit <theunsignedfairon@gmail.com>
2018-09-19 14:40:56 +0300

    + Week5/Ex1 - Seq and Paral

---
+++ week5/ex1_par.c
@@ -0,0 +1,30 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define N 10
+
+int thread_id[N];
+
+void *printFromThread() {
+    printf("Message was printing by thread with ID: %d\n", (int) pthread_self());
+    pthread_exit(NULL);
+}
+
+int main(int argc, char **argv) {
+    int rc;
+
+    for (int i = 0; i < N; i++) {
+        rc = pthread_create(&thread_id[i], NULL, printFromThread, NULL);
+
+        if (rc) {
+            printf("\n ERROR: return code from pthread_create is %d \n", rc);
+            exit(1);
+        }
+
+        printf("Thread with ID %d created by thread with ID %d\n", (int) thread_id[i], (int) pthread_self());
+    }
+
+    pthread_exit(NULL);
+}
