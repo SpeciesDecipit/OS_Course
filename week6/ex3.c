5179c6cda4cf09d3df7dacf9240173c502df9891
species_decipit <theunsignedfairon@gmail.com>
2018-09-26 12:55:14 +0300

    + Week6/Ex3

---
+++ week6/ex3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <zconf.h>
+
+
+void *signint_handler() {
+    printf("Exiting...\n");
+    exit(1);
+}
+
+int main() {
+    signal(SIGINT, (void (*)(int)) signint_handler);
+
+    while (1) {
+        printf("Going to sleep for a second...\n");
+        sleep(1);
+    }
+
+    return 0;
+}
